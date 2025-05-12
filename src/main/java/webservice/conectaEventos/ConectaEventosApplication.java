ce1030f3d0cf226c893e17df83bba0df989893f0
SamyraFigueredo <samyrafigueiredo24@gmail.com>
2025-05-12 11:57:30 -0300

    iniciando projeto

---
+++ src/main/java/webservice/conectaEventos/ConectaEventosApplication.java
@@ -0,0 +1,13 @@
+package webservice.conectaEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webservice/conectaEventos/ConectaEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package webservice.conectaEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
